52a67fca9ad6b21db48f96c418811a81687f77f3
XxJipow <jiprcroyale@gmail.com>
2024-12-01 20:40:00 +0000

    add ui

---
+++ api/ui/src/route.rs
@@ -0,0 +1,172 @@
+use hyper::{Body, Request, Response};
+use tivet_operation::prelude::*;
+
+pub struct Router;
+
+impl Router {
+	fn replace_vite_app_api_url(
+		content: &[u8],
+		config: &tivet_config::Config,
+	) -> GlobalResult<Vec<u8>> {
+		let content_str = std::str::from_utf8(content)?;
+
+		let replacement_count = content_str.matches("__APP_API_URL__").count();
+		ensure!(
+			replacement_count > 0,
+			"Expected at least one occurrence of __APP_API_URL__, found {}",
+			replacement_count
+		);
+
+		let public_origin =
+			util::url::to_string_without_slash(&config.server()?.tivet.api_public.public_origin());
+		let replaced_content = content_str.replace("__APP_API_URL__", &public_origin);
+
+		Ok(replaced_content.into_bytes())
+	}
+
+	#[doc(hidden)]
+	#[tracing::instrument(skip_all)]
+	pub async fn __inner(
+		_shared_client: chirp_client::SharedClientHandle,
+		config: tivet_config::Config,
+		_pools: tivet_pools::Pools,
+		_cache: tivet_cache::Cache,
+		_ray_id: uuid::Uuid,
+		request: &mut Request<Body>,
+		response: &mut http::response::Builder,
+		_router_config: &mut api_helper::macro_util::__RouterConfig,
+	) -> tivet_operation::prelude::GlobalResult<Option<Vec<u8>>> {
+		// Don't do anything if UI is not enabled.
+		if !config.server()?.tivet.ui.enable() {
+			return Ok(None);
+		}
+
+		let path = request.uri().path();
+
+		// Redirect to UI
+		if path == "/" || path == "/index.html" {
+			tracing::debug!(path = ?path, "redirecting to /ui/");
+			*response = std::mem::take(response)
+				.status(hyper::StatusCode::TEMPORARY_REDIRECT)
+				.header(hyper::header::LOCATION, "/ui/");
+			return Ok(Some("Redirecting to /ui/".into()));
+		}
+
+		// Check if the path starts with "/ui"
+		if !request.uri().path().starts_with("/ui") {
+			return Ok(None);
+		}
+
+		// Strip the prefix to:
+		// - Strip the mount path of /ui.
+		// - Strip the starting slash in order to match the format `include_dir` needs.
+		let path = request.uri().path().trim_start_matches("/ui/");
+		let content = tivet_hub_embed::get_file_content(path);
+
+		match content {
+			Some(content) => {
+				let content_type = mime_guess::from_path(path).first_or_octet_stream();
+				tracing::debug!(
+					path = ?path,
+					?content_type,
+					length = ?content.len(),
+					"serving file"
+				);
+				if let Some(headers) = response.headers_mut() {
+					headers.insert(
+						hyper::header::CONTENT_TYPE,
+						hyper::header::HeaderValue::from_str(content_type.as_ref()).unwrap(),
+					);
+				}
+
+				// Replace VITE_APP_API_URL if the file is index.html
+				let content = if path == "index.html" {
+					Self::replace_vite_app_api_url(content, &config)?
+				} else {
+					content.to_vec()
+				};
+
+				Ok(Some(content))
+			}
+			None => {
+				// HACK(FRONT-545): Paths with tokens in them are interpreted as a file, so we
+				// include certain exceptions
+				if path.ends_with(".html") || !path.contains('.') || path.contains("device.") {
+					tracing::debug!(
+						path = ?path,
+						"file not found, serving index.html"
+					);
+
+					// Serve index.html content
+					let index_content = unwrap!(
+						tivet_hub_embed::get_file_content("index.html"),
+						"index.html not found"
+					);
+
+					// Replace VITE_APP_API_URL in index.html
+					let index_content = Self::replace_vite_app_api_url(index_content, &config)?;
+
+					if let Some(headers) = response.headers_mut() {
+						headers.insert(
+							hyper::header::CONTENT_TYPE,
+							hyper::header::HeaderValue::from_static("text/html"),
+						);
+					}
+
+					Ok(Some(index_content))
+				} else {
+					tracing::debug!(
+						path = ?path,
+						"file not found, returning 404"
+					);
+
+					*response = std::mem::take(response).status(hyper::StatusCode::NOT_FOUND);
+					Ok(Some("Not Found".into()))
+				}
+			}
+		}
+	}
+
+	#[tracing::instrument(skip_all)]
+	pub async fn handle(
+		shared_client: chirp_client::SharedClientHandle,
+		config: tivet_config::Config,
+		pools: tivet_pools::Pools,
+		cache: tivet_cache::Cache,
+		ray_id: uuid::Uuid,
+		mut request: Request<Body>,
+		mut response: http::response::Builder,
+	) -> Result<Response<Body>, http::Error> {
+		tracing::debug!(
+			method = ?request.method(),
+			uri = ?request.uri(),
+			"received request"
+		);
+
+		let mut router_config =
+			match api_helper::macro_util::__RouterConfig::new(&config, request.uri()) {
+				Ok(x) => x,
+				Err(err) => {
+					return api_helper::error::handle_rejection(&config, err, response, ray_id)
+				}
+			};
+
+		let res = Self::__inner(
+			shared_client,
+			config.clone(),
+			pools,
+			cache,
+			ray_id,
+			&mut request,
+			&mut response,
+			&mut router_config,
+		)
+		.await;
+
+		match res {
+			Ok(Some(content)) => response.body(Body::from(content)),
+			Ok(None) => response.body(Body::empty()),
+			Err(err) => api_helper::error::handle_rejection(&config, err, response, ray_id),
+		}
+	}
+}
