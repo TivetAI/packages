348a7c09f9522b5a69d605a15708702f75526734
XxJipow <jiprcroyale@gmail.com>
2025-01-15 12:40:00 +0000

    add identity

---
+++ api/identity/src/assert.rs
@@ -0,0 +1,18 @@
+use api_helper::ctx::Ctx;
+use tivet_operation::prelude::*;
+
+use crate::auth::Auth;
+
+/// Validates that a given user ID is registered.
+pub async fn user_registered(ctx: &Ctx<Auth>, user_id: Uuid) -> GlobalResult<()> {
+	// If the user has at least one identity they are considered registered
+	let identity = op!([ctx] user_identity_get {
+		user_ids: vec![user_id.into()]
+	})
+	.await?;
+
+	let identities = &unwrap_ref!(identity.users.first()).identities;
+	ensure_with!(!identities.is_empty(), IDENTITY_NOT_REGISTERED);
+
+	Ok(())
+}
